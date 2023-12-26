05466da93a512d164c57c9aca4a1ec3881009827
hgs6424 <hgs6424@naver.com>
2023-12-26 20:10:09 +0900

    fix scope2

--- orchestration/src/main/java/com/baki/orchestration/adapter/out/persistence/event/EventRepository.java
+++ orchestration/src/main/java/com/baki/orchestration/adapter/out/persistence/event/EventRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends ReactiveMongoRepository<EventDocument, String> {
+interface EventRepository extends ReactiveMongoRepository<EventDocument, String> {
 }
